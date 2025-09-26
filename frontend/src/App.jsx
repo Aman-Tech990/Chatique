d0944217878e23ab0c89bcd8d3e489462b444fc8
Aman Parida <amantechz467@gmail.com>
2025-09-26 23:26:05 +0530

    Frontend Setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./pages/Contact";
+import HeroSection from "./pages/HeroSection";
+
+function App() {
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainElement />,
+      children: [
+        {
+          path: "/",
+          element: <HeroSection />
+        },
+        {
+          path: "contact",
+          element: <Contact />
+        }
+      ]
+    }
+  ]);
+
+  return (
+    <RouterProvider router={appRouter} />
+  )
+}
+
+export default App;
